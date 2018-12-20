92138640670417281defdda4b34c9f129291e00f
Umar Yaqub <yaqub_umar@yahoo.co.uk>
2018-12-20 13:09:05 +0000

    Initial Commit

---
+++ SHMA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SHMA
+//
+//  Created by Umar Yaqub on 20/12/2018.
+//  Copyright © 2018 SurreyHeathMuslimAssociation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
