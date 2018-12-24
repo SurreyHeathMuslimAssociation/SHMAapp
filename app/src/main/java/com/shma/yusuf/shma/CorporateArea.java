71e34ed99ff1c7f9651887223897d135c5757239
Yu5ufD <yusufdinah@gmail.com>
2018-12-24 15:41:54 +0000

    corporate area Working

---
+++ app/src/main/java/com/shma/yusuf/shma/CorporateArea.java
@@ -0,0 +1,13 @@
+package com.shma.yusuf.shma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CorporateArea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_corporate_area);
+    }
+}
