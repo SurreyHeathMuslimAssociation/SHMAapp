8899d0479200d93423f282f37551764c6b616a66
Yu5ufD <yusufdinah@gmail.com>
2019-01-13 23:50:34 +0000

    neating up code title now changes

--- app/src/main/java/com/shma/yusuf/shma/MemberArea.java
+++ app/src/main/java/com/shma/yusuf/shma/MemberArea.java
@@ -5,19 +5,45 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MemberArea extends AppCompatActivity {
+    private TextView Title;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String sessionId= getIntent().getStringExtra("EXTRA_SESSION_INFO");
+//Declarations
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_area);
-        View parentLayout = findViewById(android.R.id.content);
-        Snackbar mySnackbar;
-        mySnackbar = Snackbar.make(parentLayout, "sessionID is " + sessionId, 6000);
-        mySnackbar.show();
+        String sessionId= getIntent().getStringExtra("EXTRA_SESSION_INFO");
+//Assigning elements to variables
+        Title = findViewById(R.id.Title);
+//Functions and methods
+        checks(sessionId);
 
     }
+public void checks(String sessionId){
+    View parentLayout = findViewById(android.R.id.content);
+    String changed = "" ;
+    Snackbar mySnackbar;
+
+    mySnackbar = Snackbar.make(parentLayout, "sessionID is " + sessionId, 6000);
+    mySnackbar.show();
+
+    switch(sessionId)
+    {
+        case "Login":
+            changed = "Login" ;
+            break;
+        case "ExistMemb":
+            changed = "Existing Member" ;
+            break;
+        case "Newmemb" :
+            changed = "New Member" ;
+            break;
+    }
+    Title.setText(changed);
+}
+
 }
