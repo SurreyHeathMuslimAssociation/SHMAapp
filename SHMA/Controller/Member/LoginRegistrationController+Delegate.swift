4f8dbf75d1dfd48d49516ab53e465837f49f7cea
Umar Yaqub <yaqub_umar@yahoo.co.uk>
2019-01-19 20:25:03 +0000

    finished login register

---
+++ SHMA/Controller/Member/LoginRegistrationController+Delegate.swift
@@ -0,0 +1,26 @@
+//
+//  LoginRegistrationController+Delegate.swift
+//  SHMA
+//
+//  Created by Umar Yaqub on 18/01/2019.
+//  Copyright © 2019 SurreyHeathMuslimAssociation. All rights reserved.
+//
+
+import UIKit
+
+extension LoginRegistrationController: LoginRegistrationViewDelegate {
+    
+    func didPressLoginRegister() {
+        if didSelectLogin == true {
+            handleLogIn()
+        } else if didSelectExistingMemberRegistration == true {
+            handleExistingMemberSignUp()
+        } else {
+            determineIfMemberDetailRequirementsAreMet()
+        }
+    }
+    
+    func didSwitchToFamilyMembership() {
+        setupLoginRegistrationViewHeightOrBottomAnchor()
+    }
+}
